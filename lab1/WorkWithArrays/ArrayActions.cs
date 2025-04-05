08ace8e77d622686204982b5659316c4d01d94a7
triks-cmd <amejson73@gmail.com>
2025-04-05 14:43:56 +0300

    done

--- lab1/WorkWithArrays/ArrayActions.cs
+++ lab1/WorkWithArrays/ArrayActions.cs
@@ -2,7 +2,7 @@
 
 namespace ArrayApp
 {
-    public class ArrayActions
+    public partial class ArrayActions
     {
         public double[] MixArrays(double[] primaryArray, double[] secondaryArray, int cutIndex)
         {
@@ -58,41 +58,6 @@ namespace ArrayApp
         public double GetNum(double[] array, int index)
         {
             return index < array.Length ? array[index] : 0;
-        }
-
-        public double DoMath(double x, double y, double z)
-        {
-            const double Epsilon = 1e-7;
-            double denominator = x + y;
-
-            if (Math.Abs(denominator) < Epsilon)
-            {
-                return 0;
-            }
-
-            return (2 * Math.Sin(x) + 3 * y * Math.Pow(Math.Cos(z), 3)) / denominator;
-        }
-
-        public double AddBetweenPositives(double[] array)
-        {
-            int first = -1, last = -1;
-            for (int i = 0; i < array.Length; i++)
-            {
-                if (array[i] > 0)
-                {
-                    first = first == -1 ? i : first;
-                    last = i;
-                }
-            }
-
-            if (first == -1 || first == last) return 0;
-            
-            double sum = 0;
-            for (int i = first; i <= last; i++)
-            {
-                sum += array[i];
-            }
-            return sum;
         }
     }
 }
---
+++ lab1/WorkWithArrays/MathOperations.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ArrayApp
+{
+    public partial class ArrayActions
+    {
+        public double DoMath(double x, double y, double z)
+        {
+            const double Epsilon = 1e-7;
+            double denominator = x + y;
+
+            if (Math.Abs(denominator) < Epsilon)
+            {
+                return 0;
+            }
+
+            return (2 * Math.Sin(x) + 3 * y * Math.Pow(Math.Cos(z), 3)) / denominator;
+        }
+
+        public double AddBetweenPositives(double[] array)
+        {
+            int first = -1, last = -1;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > 0)
+                {
+                    first = first == -1 ? i : first;
+                    last = i;
+                }
+            }
+
+            if (first == -1 || first == last) return 0;
+            
+            double sum = 0;
+            for (int i = first; i <= last; i++)
+            {
+                sum += array[i];
+            }
+            return sum;
+        }
+    }
+}
